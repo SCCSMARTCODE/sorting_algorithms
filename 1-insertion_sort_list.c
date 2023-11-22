7baa900aa3f35d1786d6d5345772fcf499dc5e6e
SCCSMARTCODE <sccsmart247@gmail.com>
2023-11-21 16:05:52 -0800

    sorting

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,11 +8,12 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *head, *current;
+	int run = 0;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *head, *current;
-	int run = 0;
 
 	head = *list;
 
