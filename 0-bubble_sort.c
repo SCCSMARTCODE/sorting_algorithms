41da2d87900511d6df9b6e888511fba35b434a1a
SCCSMARTCODE <sccsmart247@gmail.com>
2023-11-21 16:53:35 -0800

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order using bubble sort.
+ * bubble_sort - Sorts an array of integers.
  * @array: Array to be sorted.
  * @size: Size of the array.
  *
@@ -9,23 +9,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, j;
-        int temp;
+	size_t i, j;
+	int temp;
 
-        if (size < 2 || array == NULL)
-                return;
+	if (size < 2 || array == NULL)
+		return;
 
-        for (j = 0; j < size; j++)
-        {
-                for (i = 0; i < size - 1; i++)
-                {
-                        if (array[i] > array[i + 1])
-                        {
-                                temp = array[i];
-                                array[i] = array[i + 1];
-                                array[i + 1] = temp;
-                                print_array(array, size);
-                        }
-                }
-        }
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
