bdca656b043de81a972d77fbca2ab46a9b783490
SCCSMARTCODE <sccsmart247@gmail.com>
2023-12-13 05:31:02 -0800

    shell update

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,17 +1,19 @@
 #include "sort.h"
-
 /**
- * shell_sort - Sorts an array of integers in ascending order
- *              using the Shell sort algorithm.
- * @array: The array to be sorted.
- * @size: The size of the array.
- */
+ * shell_sort -Sort an array using shell_sort algorithm
+ * @array: array
+ * @size: size of the array
+ * Return: NULL
+ **/
 void shell_sort(int *array, size_t size)
 {
-	unsigned  long int gap = 1;
-	size_t i, j;
+	unsigned int gap = 1, i, j;
 	int temp;
 
+	if (array == NULL)
+		return;
+	if (size < 2)
+		return;
 	while (gap < size / 3)
 		gap = gap * 3 + 1;
 
@@ -21,16 +23,14 @@ void shell_sort(int *array, size_t size)
 		{
 			temp = array[i];
 			j = i;
-
 			while (j >= gap && array[j - gap] > temp)
 			{
 				array[j] = array[j - gap];
 				j -= gap;
 			}
-
 			array[j] = temp;
 		}
 		print_array(array, size);
-		gap = (gap - 1) / 3;
+		gap /= 3;
 	}
 }
